cb798892fc31f9fa6e3fae4ecaa0b62f6410af4e
osamikoyo <v.pylayev@mail.ru>
2025-02-24 07:10:06 +0500

    add server funcs

--- internal/data/models/models.go
+++ internal/data/models/models.go
@@ -1,7 +1,28 @@
 package models
 
+import "github.com/osamikoyo/IM-review/pkg/proto/pb"
+
 type Review struct{
 	ProductID uint64
 	Mark uint8
 	Content string
+}
+
+func ToModels(r *pb.Review) *Review {
+	return &Review{
+		ProductID: r.ProductID,
+		Mark: uint8(r.Mark),
+		Content: r.Content,
+	}
+}
+
+func ToPB(r []Review) []*pb.Review {
+	var res []*pb.Review
+	for _, rs := range r{
+		res = append(res, &pb.Review{
+			ProductID: rs.ProductID,
+			Content: rs.Content,
+			Mark: uint32(rs.Mark),
+		})
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,49 @@
 package server
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/osamikoyo/IM-review/internal/data"
+	"github.com/osamikoyo/IM-review/internal/data/models"
+	"github.com/osamikoyo/IM-review/pkg/proto/pb"
+)
+
+type Server struct{
+	pb.UnimplementedReviewServiceServer
+	Storage *data.Storage
+}
+
+func (s *Server) Add(_ context.Context, req *pb.AddReviewReq) (*pb.Response, error){
+	err := s.Storage.Add(models.ToModels(req.Review))
+	if err != nil{
+		return &pb.Response{
+			Error: err.Error(),
+			Status: http.StatusInternalServerError,
+		}, err
+	}
+
+	return &pb.Response{
+		Error: "",
+		Status: http.StatusOK,
+	}, nil
+}
+func (s *Server) Get(_ context.Context, req *pb.GetReviewsReq) (*pb.GetReviewsResp, error){
+	reviews, err := s.Storage.Get(req.ProductID)
+	if err != nil{
+		return &pb.GetReviewsResp{
+			Response: &pb.Response{
+				Error: err.Error(),
+				Status: http.StatusInternalServerError,
+			},
+		}, err
+	}
+
+	return &pb.GetReviewsResp{
+		Reviews: models.ToPB(reviews),
+		Response: &pb.Response{
+			Error: "",
+			Status: http.StatusOK,
+		},
+	}, nil
+}
