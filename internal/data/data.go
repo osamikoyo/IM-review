c4989eb0b63ea8cbbe2b514cf1f123ee06b63b3b
osamikoyo <v.pylayev@mail.ru>
2025-02-24 07:00:53 +0500

    data final

--- internal/data/data.go
+++ internal/data/data.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"github.com/osamikoyo/IM-review/internal/config"
+	"github.com/osamikoyo/IM-review/internal/data/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -16,5 +17,21 @@ func New(cfg *config.Config) (*Storage, error) {
 		return nil, err
 	}
 
-	
+	err = db.AutoMigrate(&models.Review{})
+
+	return &Storage{db: db}, err	
+}
+
+func (s *Storage) Add(review *models.Review) error {
+	return s.db.Create(review).Error
+}
+
+func (s *Storage) Get(id uint64) ([]models.Review, error) {
+	var reviews []models.Review
+
+	result := s.db.Where(&models.Review{
+		ProductID: id,
+	}).Find(&reviews)
+
+	return reviews, result.Error
 }
